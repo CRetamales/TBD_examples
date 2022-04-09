f77528854364839c63adbf852dac389196764521
Bastán loyola <bastian.loyola@usach.cl>
2022-04-09 19:13:02 -0400

    Obtencion de datos desde base de datos

--- ejemplo2/src/main/java/cl/tbd/ejemplo2/Ejemplo2Application.java
+++ ejemplo2/src/main/java/cl/tbd/ejemplo2/Ejemplo2Application.java
@@ -1,5 +1,7 @@
 package cl.tbd.ejemplo2;
 
+import java.sql.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.sql2o.Connection;
@@ -16,6 +18,9 @@ public class Ejemplo2Application {
 		try{
 			Connection con = base.open();
 			System.out.println("si funciona");
+			String sql = "SELECT count(table1.*) AS cantidad FROM (SELECT film.film_id AS ide, rental.rental_date AS fecha FROM film INNER JOIN inventory ON film.film_id = inventory.film_id INNER JOIN rental ON inventory.inventory_id = rental.inventory_id INNER JOIN customer ON customer.customer_id = rental.customer_id WHERE customer.customer_id = 481 ORDER BY rental.rental_date DESC) table1 INNER JOIN film_category ON film_category.film_id = table1.ide INNER JOIN category ON category.category_id = film_category.category_id GROUP BY category.name;";
+			int cantidad = con.createQuery(sql).executeScalar(Integer.class);
+			System.out.println("El numero de peliculas es: " + cantidad);
 		}
 		catch(Sql2oException e){
 			System.out.println("no funciona");
