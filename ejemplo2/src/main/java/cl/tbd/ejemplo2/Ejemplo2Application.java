0ca371797b3009ae22fd308d323820b84cae2dbe
Bastán loyola <bastian.loyola@usach.cl>
2022-04-09 18:26:03 -0400

    Correccion conexion base de datos

--- ejemplo2/src/main/java/cl/tbd/ejemplo2/Ejemplo2Application.java
+++ ejemplo2/src/main/java/cl/tbd/ejemplo2/Ejemplo2Application.java
@@ -2,6 +2,7 @@ package cl.tbd.ejemplo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
 import cl.tbd.ejemplo2.repositories.DatabaseContext;
@@ -11,8 +12,9 @@ public class Ejemplo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ejemplo2Application.class, args);
+		Sql2o base = DatabaseContext.sql2o();
 		try{
-			Sql2o base = DatabaseContext.sql2o();
+			Connection con = base.open();
 			System.out.println("si funciona");
 		}
 		catch(Sql2oException e){
